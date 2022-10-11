776d603a5f0739bf67d7e6e32e0d0d1c04269bec
Nik Bailey <bail0301@morris.umn.edu>
2022-10-11 16:26:02 -0500

    Still working one getting disemvowel

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <string.h>
 #define BUF_SIZE 1024
 
 bool is_vowel(char c) {
@@ -9,7 +10,7 @@ bool is_vowel(char c) {
      * false otherwise.
      */
 	char* vowels = calloc(11, sizeof(char));
-	strcopy(vowels, "aeiouAEOIOU\0");
+	strcpy(vowels, "aeiouAEOIOU\0");
 	return strchr(vowels, c);
 }
 
@@ -20,6 +21,15 @@ int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
      * and this function should return the number of non-vowels that
      * that were copied over.
      */
+
+	int num_non_vowels = 0;
+	for (int i = 0; i < num_chars; i++){
+		if (is_vowel(in_buf[i]) != 0){
+			out_buf[num_non_vowels] = in_buf[i];
+			num_non_vowels++;
+		}
+	}
+	return num_non_vowels;
 }
 
 void disemvowel(FILE* inputFile, FILE* outputFile) {
@@ -30,22 +40,32 @@ void disemvowel(FILE* inputFile, FILE* outputFile) {
      * use fwrite to write that out.
      */
 	FILE *input;
-	input = fopen(inputFile, "r");
-	output = fopen(outputFile, "r+");
+	FILE *output;
+	input = inputFile;
+	output = outputFile;
 	int c;
 	int counter = 0;
-	char* in_buf[BUF_SIZE];
-	char* out_buf[BUF_SIZE];
+	char in_buf[BUF_SIZE];
+	char out_buf[BUF_SIZE];
 	while(counter < BUF_SIZE) {
 		c = fgetc(input);
+		if (c == EOF){
+			int output_length = copy_non_vowels(counter, in_buf, out_buf);
+                        for (int i = 0; i < output_length; i++){
+                                fputc(out_buf[i], output);
+                        }
+                        counter = BUF_SIZE;
+		}
+		else {
 		in_buf[counter] = (char) c;
 		counter++;
-		if (counter == BUF_SIZE - 1 || c == EOF){
-			int output_length = copy_non_vowels(counter, in_buf, out_buf);
-			for (int i = 0; i < output_length; i++){
-				fputc(out_buf[i], outputFile);
+			if (counter == BUF_SIZE - 1){
+				int output_length = copy_non_vowels(counter, in_buf, out_buf);
+				for (int i = 0; i < output_length; i++){
+					fputc(out_buf[i], output);
+				}
+				counter = 0;
 			}
-			counter = 0;
 		}
 	}
 
@@ -58,13 +78,12 @@ int main(int argc, char *argv[]) {
     // provides files names as command line arguments.
     FILE *inputFile = stdin;
     FILE *outputFile = stdout;
-    
     if(argc == 3) {
-	inputFile = argv[1];
-	outputFile = argv[2];
+	inputFile = fopen(argv[1], "r");
+	outputFile = fopen(argv[2], "r+");
     }
     else if(argc == 2) {
-	inputFile = argv[1];
+	inputFile = fopen(argv[1], "r");
     }
     // Code that processes the command line arguments
     // and sets up inputFile and outputFile.
