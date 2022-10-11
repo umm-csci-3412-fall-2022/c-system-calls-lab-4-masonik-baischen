257059e9aa7d4bca74f28d29a82ba67e1dd2eb28
Nik Bailey <bail0301@morris.umn.edu>
2022-10-11 15:53:52 -0500

    Working on task 1

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,25 +1,75 @@
-#include <stdlib.h>
-#include <string.h>
-#include "disemvowel.h"
+#include <stdio.h>
+#include <stdbool.h>
 
-char *disemvowel(char *str) {
-  char *consonants = calloc(strlen(str)+1, sizeof(char));
-  char *vowels = calloc(11, sizeof(char));
-  strcpy(vowels, "aeiouAEIOU\0");
-  int j = 0;
-  for(int i = 0; i < strlen(str); ++i) {
-        if(strchr(vowels, str[i]) == NULL) {
-                consonants[j++] = str[i];
-        }
-  }
-  consonants[j] = '\0';
+#define BUF_SIZE 1024
 
-  char *result = calloc(strlen(consonants)+1, sizeof(char));
-  strcpy(result, consonants);
-  result[j] ='\0';
-
-  free((char*) consonants);
-  free((char*) vowels);
-  return result;
+bool is_vowel(char c) {
+    /*
+     * Returns true if c is a vowel (upper or lower case), and
+     * false otherwise.
+     */
+	char* vowels = calloc(11, sizeof(char));
+	strcopy(vowels, "aeiouAEOIOU\0");
+	return strchr(vowels, c);
 }
 
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+    /*
+     * Copy all the non-vowels from in_buf to out_buf.
+     * num_chars indicates how many characters are in in_buf,
+     * and this function should return the number of non-vowels that
+     * that were copied over.
+     */
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+    /*
+     * Copy all the non-vowels from inputFile to outputFile.
+     * Create input and output buffers, and use fread() to repeatedly read
+     * in a buffer of data, copy the non-vowels to the output buffer, and
+     * use fwrite to write that out.
+     */
+	FILE *input;
+	input = fopen(inputFile, "r");
+	output = fopen(outputFile, "r+");
+	int c;
+	int counter = 0;
+	char* in_buf[BUF_SIZE];
+	char* out_buf[BUF_SIZE];
+	while(counter < BUF_SIZE) {
+		c = fgetc(input);
+		in_buf[counter] = (char) c;
+		counter++;
+		if (counter == BUF_SIZE - 1 || c == EOF){
+			int output_length = copy_non_vowels(counter, in_buf, out_buf);
+			for (int i = 0; i < output_length; i++){
+				fputc(out_buf[i], outputFile);
+			}
+			counter = 0;
+		}
+	}
+
+
+}
+
+int main(int argc, char *argv[]) {
+    // This sets these to `stdin` and `stdout` by default.
+    // You then need to set them to user specified files when the user
+    // provides files names as command line arguments.
+    FILE *inputFile = stdin;
+    FILE *outputFile = stdout;
+    
+    if(argc == 3) {
+	inputFile = argv[1];
+	outputFile = argv[2];
+    }
+    else if(argc == 2) {
+	inputFile = argv[1];
+    }
+    // Code that processes the command line arguments
+    // and sets up inputFile and outputFile.
+    
+    disemvowel(inputFile, outputFile);
+
+    return 0;
+}
